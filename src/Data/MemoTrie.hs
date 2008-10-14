a6ab271c68b82b45d0d1398a81bbb02bec80ae22
Conal Elliott <conal@conal.net>
2008-10-13 17:20:08 -0700

    ScopedTypeVariables -- bug work-around
    darcs-hash:20081014002008-fb517-7382199b9730c6d67dbf937754c354f0f230e691

--- src/Data/MemoTrie.hs
+++ src/Data/MemoTrie.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE GADTs, TypeFamilies, TypeOperators #-}
+{-# LANGUAGE GADTs, TypeFamilies, TypeOperators, ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall -frewrite-rules #-}
+-- ScopedTypeVariables works around a 6.10 bug.  The forall keyword is
+-- supposed to be recognized
+
 ----------------------------------------------------------------------
 -- |
 -- Module      :  Data.MemoTrie
