8bc81d472549bb86a76ff91b76dfa7f291939cdf
Conal Elliott <conal@conal.net>
2016-09-11 12:06:47 -0700

    color example

--- examples/Generic.hs
+++ examples/Generic.hs
@@ -2,25 +2,24 @@
 import Data.MemoTrie
 import GHC.Generics (Generic) 
 
-data Color 
- = RGB Int Int Int
- | NamedColor String 
- deriving (Generic) 
+data Color = RGB Int Int Int
+           | NamedColor String 
+  deriving (Generic) 
 
 instance HasTrie Color where
- newtype (Color :->: b) = ColorTrie { unColorTrie :: Reg Color :->: b } 
- trie = trieGeneric ColorTrie 
- untrie = untrieGeneric unColorTrie
- enumerate = enumerateGeneric unColorTrie
+  newtype (Color :->: b) = ColorTrie { unColorTrie :: Reg Color :->: b } 
+  trie      = trieGeneric ColorTrie 
+  untrie    = untrieGeneric unColorTrie
+  enumerate = enumerateGeneric unColorTrie
 
 runColor (RGB r g b) = r + g + b
 runColor (NamedColor s) = length [1..10e7] 
 
 runColorMemoized = memo runColor 
 
-main = do 
- putStrLn "first call (should take a few seconds): " 
- print$ runColorMemoized (NamedColor "")
- putStrLn "cached call (should be instantaneous): " 
- print$ runColorMemoized (NamedColor "") 
+main =
+  do putStrLn "first call (should take a few seconds): " 
+     print$ runColorMemoized (NamedColor "")
+     putStrLn "cached call (should be instantaneous): " 
+     print$ runColorMemoized (NamedColor "") 
 
